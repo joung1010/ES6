cbc2ce27a245bafba01f9c426a17365b0a5eb331
mason park <joung4342@gmail.com>
2022-04-26 21:33:35 +0900

    Naming

---
+++ Basic/1.naming.js
@@ -0,0 +1,31 @@
+/* 변수 큐칙!
+*  라틴문자(0-9,a-z,A-Z), _
+*  대소문자를 구분
+*  추천 : camelCase (likeThis)
+*  한국어 X
+*  예약어 X( let if , let for)
+*  숫자로 시작 X
+*  특수문자 X
+*  여러개의 변수를 1, 2, 3 숫자로 구분 X -> 최대한 의미있게, 구체적인 이름으로 작성
+*  */
+
+let apple;
+let redApple;
+
+// 나쁜 예제
+let num = 20 ;
+
+// 좋은 예제
+let myAge = 20;
+
+// 나쁜 예제
+let audio1;
+let audio2;
+
+// 좋은 예제
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁
+let audioBackgound;
+let audioWind;

8817f42454d9e4581543175509eb4d98cc1d45c4
mason park <joung4342@gmail.com>
2022-04-29 08:28:00 +0900

    conditional statement

---
+++ Basic/Control_flow_statement/if.js
@@ -0,0 +1,21 @@
+// 조건문 conditional statement
+// if(조건) {  }
+// if(조건) {  } else {   }
+// if(조건) {  } else if (조건) {   } else {  }
+
+let fruit = 'orange';
+if (fruit === 'apple') {
+    console.log('🍎');
+} else if (fruit === 'orange') {
+    console.log('🍊');
+} else {
+    console.log('!!');
+}
+
+if (flase) {
+    console.log('출력하면 안됨')
+}
+// if 조건문이 false 가 되면 된다 -> false, 0 , null, undefined , ''
+// 즉 if 조건문은 true 나 false 로 표현되는 표현식이 출력력
+
+//MDN if: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Statements/if...else

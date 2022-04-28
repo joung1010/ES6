aee43a6a02fef6aa5970e5ea610b17b11c077132
mason park <joung4342@gmail.com>
2022-04-28 19:02:19 +0900

    Increment & Decrement Operators

---
+++ Basic/Operator/1.5_imcrement.js
@@ -0,0 +1,26 @@
+// 증가 & 감소 연산자 Increment & Decrement Operators
+
+let a = 0;
+console.log(a);
+a++;  //a = a + 1;
+console.log(a);
+a--; // a= a - 1 ;
+console.log(a);
+
+console.clear();
+// 주의 !
+// a++ 필요한 연산을 하고 , 그 뒤 값을 증가시킴
+// ++a 값을 먼저 증가하고, 필요한 연산을 함
+a = 0;
+let b = a++;
+console.log(b); // 0
+console.log(a); // 1
+
+a = 0;
+let c = ++a;
+console.log(c);
+console.log(a)
+
+a = 0;
+console.log(a++); // 출력하고(필요한 연산) 값을 증가 시킴 
+console.log(a);

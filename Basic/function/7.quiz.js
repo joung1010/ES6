84ce11d8abca4892609e9a2e87be023b8c58ad8d
mason park <joung4342@gmail.com>
2022-04-29 14:54:38 +0900

    Quiz

---
+++ Basic/function/7.quiz.js
@@ -0,0 +1,29 @@
+// 주어진 숫자 만큼 0 부터 순회하는 함수
+// 순회하면서 주어진 특정한 일을 수행해야 함
+// 5 , 순회하는 숫자를 다 출력하고 싶음
+// 5, 순회하는 숫자의 두배값을 출력하고 싶음
+// function iterate(max, action);
+
+function iterate(max, action) {
+    for (let i = 0; i < max; i++) {
+        action(i);
+    }
+}
+
+function log(num) {
+    console.log(num);
+}
+
+function double(num) {
+    console.log(num * 2)
+}
+
+
+
+iterate(3, (num)=> console.log(num));
+iterate(3, (num)=> console.log(num*2) );
+
+
+setTimeout(()=>{
+    console.log('1초 뒤 실행할꺼야!!')
+}, 1000);

339d7e234206ced7a84f9511b5ca75adae110797
mason park <joung4342@gmail.com>
2022-05-01 15:37:28 +0900

    Boolean Object

---
+++ Basic/7.Built_in_Objects/3.boolean.js
@@ -0,0 +1,23 @@
+//Boolean
+const isTrue = new Boolean(true);
+console.log(isTrue.valueOf());
+
+/*
+*   Falshy
+*   0
+*   -0
+*   undefined
+*   null
+*   NaN
+*   ''
+* */
+
+/*
+*   Truthy
+*   1
+*   -1
+*   '0'
+*   'false'
+*   []
+*   {}
+* */

669949fd42cc4301ae356bcc18d5d830a980f425
mason park <joung4342@gmail.com>
2022-05-03 18:55:28 +0900

    Array Method2

--- Basic/8.DataStructure/1.3_array-method.js
+++ Basic/8.DataStructure/1.3_array-method.js
@@ -44,3 +44,51 @@ console.log(deleted); //[ 'banana' ] // 배열 형태로 반환
 fruits.splice(1, 0, 'banana','strawberry');
 console.log(fruits); //[ 'apple', 'banana', 'strawberry', 'peach' ]
 
+// 잘라진 새로운 배열을 만듬(slice)
+// 아무 값도 지정하지 않으면 0 부터 슬라이스함
+
+let newArr = fruits.slice(0, 2);
+console.log(newArr);
+console.log(fruits);
+ newArr = fruits.slice();
+console.log(newArr);
+
+//여러개의 배열을 붙여줌(concat)
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = arr1.concat(arr2);
+console.log(arr1);//[ 1, 2, 3 ]
+console.log(arr2);// [ 4, 5, 6 ]
+console.log(arr3);//[ 1, 2, 3, 4, 5, 6 ]
+
+// 순서를 거꾸로
+const arr4 = arr3.reverse();
+console.log(arr4);//[ 6, 5, 4, 3, 2, 1 ]
+
+console.clear();
+// 중첩된 배열을 하나의 배열로 쫙 펴기(기본1단계)
+let arr = [
+  [1,2,3],
+  [4,[5,6]],
+];
+console.log(arr);
+console.log(arr.flat()); // [ 1, 2, 3, 4, [ 5, 6 ] ]
+console.log(arr.flat(2));//[ 1, 2, 3, 4, 5, 6 ]
+
+arr = arr.flat(2);
+// 특정한 값으로 배열을 채우기
+arr.fill(0);
+console.log(arr); // [ 0, 0, 0, 0, 0, 0 ]
+
+arr.fill('s',1,3);
+console.log(arr);//[ 0, 's', 's', 0, 0, 0 ]
+
+arr.fill('a',1);
+console.log(arr);
+
+//배열을 문자열로 합하기
+let text = arr.join();
+console.log(text); // 0,a,a,a,a,a
+text = arr.join(' | ');
+console.log(text);//0 | a | a | a | a | a
+
